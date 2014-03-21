a66df6bc1b7ba7d662c48376d8c9ed64ec8d69c2
yancyn <yancyn@gmail.com>
2014-03-22 00:11:11 +0800

    Able to parse json value from Fedex response.

--- ParcelTrack/src/main/java/com/muje/parcel/Fedex.java
+++ ParcelTrack/src/main/java/com/muje/parcel/Fedex.java
@@ -4,9 +4,13 @@ import android.util.Log;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -14,6 +18,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -52,10 +57,29 @@ public class Fedex extends Courier {
         packageRequest.put("trackingInfoList", infoArray);
         map.put("TrackPackagesRequest", packageRequest);
 
-        Log.d("DEBUG", map.toString());
+        //Log.d("DEBUG", map.toString());
         return map;
     }
+    private HttpResponse postJson(String path) throws Exception {
 
+        DefaultHttpClient httpClient = new DefaultHttpClient();
+        HttpPost httpPost = new HttpPost(path);
+
+        JSONObject holder = new JSONObject(composeDataJSON());
+
+        ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("action", "trackpackages"));
+        params.add(new BasicNameValuePair("data", holder.toString()));
+        params.add(new BasicNameValuePair("format", "json"));
+        params.add(new BasicNameValuePair("locale", "en_US"));
+        params.add(new BasicNameValuePair("version", "99"));
+        httpPost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+
+        httpPost.setHeader("Content-type", "application/x-www-form-urlencoded; charset=UTF-8");
+        httpPost.setHeader("X-Requested-With", "XMLHttpRequest");
+
+        return httpClient.execute(httpPost);
+    }
     private HttpResponse postJson(String path, Map params) throws Exception {
 
         DefaultHttpClient httpClient = new DefaultHttpClient();
@@ -67,37 +91,48 @@ public class Fedex extends Courier {
 
         httpPost.setHeader("Content-type", "application/x-www-form-urlencoded; charset=UTF-8");
         httpPost.setHeader("X-Requested-With", "XMLHttpRequest");
-        //httpPost.setHeader("Content-Type", "application/json");
-        //httpPost.setHeader("Accept-Encoding", "application/json");
 
-        return httpClient.execute(httpPost);//, responseHandler);
+        return httpClient.execute(httpPost);
+    }
+    private void getValueFromJSON(JSONObject json, String key) throws Exception {
+
+        Iterator iter = json.keys();
+        while(iter.hasNext()) {
+            String k = (String)iter.next();
+            Object obj = json.get(k);
+            Log.d("DEBUG", k + ": " + obj);
+            if(k.equals(key)) {
+                holder = obj;
+                return;
+            }
+
+            if(obj instanceof JSONObject) {
+                getValueFromJSON((JSONObject)obj, key);
+            } else if(obj instanceof JSONArray) {
+                getValueFromJSONArray((JSONArray)obj, key);
+            }
+        }
+    }
+    private void getValueFromJSONArray(JSONArray array, String key) throws Exception {
+
+        for(int i=0;i<array.length();i++) {
+            Object obj = array.get(i);
+            if(obj instanceof JSONObject) {
+                getValueFromJSON((JSONObject)obj, key);
+            } else if(obj instanceof JSONArray) {
+                getValueFromJSONArray((JSONArray)obj, key);
+            }
+        }
     }
 
+    private Object holder = null;
     @Override
     public void trace(String consignmentNo) throws Exception {
-
-        // TODO: Fedex trace logic
         this.consignmentNo = consignmentNo;
-
         this.tracks.clear();
 
-        // send http request
-        /*String queryString = String.format(
-                "https://www.fedex.com/fedextrack/index.html?tracknumbers=%s",
-                consignmentNo);
-        DefaultHttpClient httpClient = new DefaultHttpClient(new BasicHttpParams());
-        HttpPost httpPost = new HttpPost(queryString);
-        httpPost.setHeader("Content-type", "application/json");
-        */
-
-        Map<String, Object> map = new HashMap<String, Object>();
-        map.put("action", "trackpackages");
-        map.put("data", composeDataJSON());
-        map.put("format", "json");
-        map.put("locale", "en_MY");
-        map.put("version", 99);
-
-        HttpResponse response = postJson("https://www.fedex.com/trackingCal/track", map);// httpClient.execute(httpPost);
+        // TODO: Fedex trace logic
+        HttpResponse response = postJson("https://www.fedex.com/trackingCal/track");
         HttpEntity entity = response.getEntity();
         InputStream inputStream = entity.getContent();
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
@@ -106,13 +141,13 @@ public class Fedex extends Courier {
         String line = "";
         StringBuilder sb = new StringBuilder();
         while( (line = reader.readLine()) != null) {
-            sb.append(line + "\n");
+            sb.append(line);
         }
-        result = sb.toString(); // http result
-        Log.d("DEBUG", result);
+        result = sb.toString();
 
         JSONObject resultJson = new JSONObject(result);
-        Log.d("DEBUG", resultJson.toString());
+        getValueFromJSON(resultJson, "shipperCity");
+        Log.d("DEBUG", "Shipper City: " + holder);
     }
 
 }
