d9f74568517a84cdd34d71ad516bb46bdc07474a
yancyn <yancyn@gmail.com>
2014-04-02 15:11:34 +0800

    No code changes. Removed empty line.

--- ParcelTrack/src/main/java/com/muje/parcel/ShipmentManager.java
+++ ParcelTrack/src/main/java/com/muje/parcel/ShipmentManager.java
@@ -11,7 +11,6 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-//import android.util.Log;
 
 /**
  * Manager class for shipments.
